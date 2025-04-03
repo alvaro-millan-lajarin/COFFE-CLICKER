9cb4397deb3062c0dfcf5379cd7a4dd3cf9ec739
alvaro <alvaro.millan@students.salle.url.edu>
2025-04-03 11:13:57 +0200

    g

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import Presenstation.Controller.MainController;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         MainController mainController = new MainController();
         mainController.run();
-    }
 
+    }
 }
