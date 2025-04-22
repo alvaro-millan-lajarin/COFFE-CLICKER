3ab585aaf4f181d17c787480e097c4325ce4befc
alvaro <alvaro.millan@students.salle.url.edu>
2025-04-22 20:31:22 +0200

    resume, signup, delete..

--- src/Presenstation/Controller/GameCreationController.java
+++ src/Presenstation/Controller/GameCreationController.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameCreationController extends Controller {
@@ -68,8 +69,10 @@ public class GameCreationController extends Controller {
 
             if (existingUserByEmail != null && existingUserByEmail.getPassword().equals(password)) {
                 game = new Game(1,existingUserByEmail.getId(),nombreGame,fechaYHoraActual,fechaYHoraActual,0);
-                manageGame.setGame(game);
                 sqlGameDAO.addGame(game);
+
+                setGame(sqlGameDAO, nombreGame, existingUserByEmail);
+
                 mainController.nextScene(Scenes.GAME);
             }
 
@@ -128,4 +131,19 @@ public class GameCreationController extends Controller {
         }
         mainController.nextScene(Scenes.MENU);
     }
+    public void setGame(SQLGameDAO sqlGameDAO, String nombreGame, User user) {
+        List<Game> gamesArray = sqlGameDAO.getAllGames();
+
+
+
+        for (Game game : gamesArray) {
+            if (game.getIdUser() == user.getId() && game.getNombre().equals(nombreGame)) {
+                Game gameCopy = sqlGameDAO.getGame(game.getId());
+                manageGame.setGame(gameCopy);
+            }
+        }
+
+
+    }
+
 }
